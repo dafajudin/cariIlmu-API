960aa74be53b9691726b654a24ba2e968fee4a50
Judin <dafajudin@student.telkomuniversity.ac.id>
2023-02-11 11:57:36 +0700

    update routes, add middleware authentication

--- routes/routes.go
+++ routes/routes.go
@@ -1,42 +1,42 @@
 package routes
 
 import (
-	"github.com/labstack/echo/v4"
 	"cariIlmu-API/controller"
+	"cariIlmu-API/middleware"
+	"github.com/labstack/echo/v4"
 )
 
 func GetApiRoutes() *echo.Echo {
 	e := echo.New()
 
 	//routes for course categories
-	e.GET("/api/course_categories", controller.GetAll)
+	e.GET("/api/course_categories", controller.GetAll, middleware.IsAuthenticated)
 	e.POST("/api/course_categories", controller.PostCourseCategories)
 	e.PUT("/api/course_categories", controller.UpdateCourseCategories)
 	e.DELETE("/api/course_categories", controller.DeleteCourse_Categories)
 
-	//create course 
-	e.GET("/courses", controller.ReadAllCourses)
+	//create course
+	e.GET("/courses", controller.ReadAllCourses, middleware.IsAuthenticated)
 	e.POST("/courses", controller.CreateCourses)
 	e.PUT("/courses", controller.UpdateCourses)
 	e.DELETE("/api/courses", controller.DeleteCourses)
 
 	//routes for login
-	e.POST("/api/login", controller.CheckLoginAdmin)
+	e.POST("/login", controller.CheckLogin)
 	e.GET("/generate-hash/:password", controller.GenerateHashPassword)
 
-	//routes for users_courses 
-	e.GET("/api/users_courses", controller.ReadAllUsersCourses)
+	//routes for users_courses
+	e.GET("/api/users_courses", controller.ReadAllUsersCourses, middleware.IsAuthenticated)
 	e.POST("api/users_courses", controller.CreateUsersCourses)
 	e.PUT("/api/users_courses", controller.UpdateUsersCourses)
 	e.DELETE("/api/users_courses/:id", controller.DeleteUsersCourses)
 
 	//routes for users
-	e.GET("/api/users", controller.ReadAllUsers)
+	e.GET("/api/users", controller.ReadAllUsers, middleware.IsAuthenticated)
 	e.POST("/api/users", controller.CreateUsers)
 	e.PUT("/api/users", controller.UpdateUsers)
 	e.DELETE("/api/users", controller.DeleteUsers)
 
 	//start server
-
 	return e
 }
