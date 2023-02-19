4fa4c9e12177488116d90f62cf4c0bc862b6fa8b
Judin <dafajudin@student.telkomuniversity.ac.id>
2023-02-19 23:11:00 +0700

    edit routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"cariIlmu-API/controller"
-	"cariIlmu-API/middleware"
+	// "cariIlmu-API/middleware"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,15 +10,15 @@ func GetApiRoutes() *echo.Echo {
 	e := echo.New()
 
 	//routes for course categories
-	e.GET("/api/course_categories", controller.GetAll, middleware.IsAuthenticated)
-	e.POST("/api/course_categories", controller.PostCourseCategories)
+	e.GET("/api/course_categories", controller.ReadAllCourseCategories)
+	e.POST("/api/course_categories", controller.CreateCourseCategories)
 	e.PUT("/api/course_categories", controller.UpdateCourseCategories)
-	e.DELETE("/api/course_categories", controller.DeleteCourse_Categories)
+	e.DELETE("/api/course_categories", controller.DeleteCourseCategories)
 
 	//create course
-	e.GET("/courses", controller.ReadAllCourses, middleware.IsAuthenticated)
-	e.POST("/courses", controller.CreateCourses)
-	e.PUT("/courses", controller.UpdateCourses)
+	e.GET("/api/courses", controller.ReadAllCourses)
+	e.POST("/api/courses", controller.CreateCourses)
+	e.PUT("/api/courses", controller.UpdateCourses)
 	e.DELETE("/api/courses", controller.DeleteCourses)
 
 	//routes for login
@@ -26,13 +26,13 @@ func GetApiRoutes() *echo.Echo {
 	e.GET("/generate-hash/:password", controller.GenerateHashPassword)
 
 	//routes for users_courses
-	e.GET("/api/users_courses", controller.ReadAllUsersCourses, middleware.IsAuthenticated)
+	e.GET("/api/users_courses", controller.ReadAllUsersCourses)
 	e.POST("api/users_courses", controller.CreateUsersCourses)
 	e.PUT("/api/users_courses", controller.UpdateUsersCourses)
-	e.DELETE("/api/users_courses/:id", controller.DeleteUsersCourses)
+	e.DELETE("/api/users_courses", controller.DeleteUsersCourses)
 
 	//routes for users
-	e.GET("/api/users", controller.ReadAllUsers, middleware.IsAuthenticated)
+	e.GET("/api/users", controller.ReadAllUsers)
 	e.POST("/api/users", controller.CreateUsers)
 	e.PUT("/api/users", controller.UpdateUsers)
 	e.DELETE("/api/users", controller.DeleteUsers)
